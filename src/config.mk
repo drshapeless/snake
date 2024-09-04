156e6ce3bfec6903dfd3e31d166242ee81e1485c
Jacky Li <drsl@drshapeless.com>
2024-09-04 23:26:30 +0800

    Add vma

--- src/config.mk
+++ src/config.mk
@@ -2,11 +2,13 @@ BIN = snake
 
 # compiler
 CC = clang
+CXX = clang++
 
 # includes and libs
 INCS = $(shell pkg-config --cflags sdl3)
-LIBS = $(shell pkg-config --libs sdl3)
+LIBS = $(shell pkg-config --libs sdl3) -lvulkan -lstdc++
 
 # flags
 CFLAGS = $(INCS) -O2 -std=c18 -g
+CXXFLAGS = $(INCS) -O2 -std=c++14 -Wno-nullability-completeness
 LDFLAGS = $(LIBS)
